1f7c353a21159275f320efd3fca28250f6454cc5
riomonet <arizablozki@gmail.com>
2023-08-18 08:44:48 -0400

    modifed EightBall.js

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Answers from './answers'
 import './EightBall.css'
 
 const EightBall = () => {
-    return <div id="EightBall"> Think of a Question </div>
+
+    const getAnswer = () => {
+	const idx = Math.floor(Math.random() * Answers.length)
+	return Answers[idx]
+    }
+    
+    const postReply = () => {
+	let answer = getAnswer();
+	setColor(answer.color)
+	setMsg(answer.msg)
+    }
+
+    const [color, setColor] = useState('black')
+    const [msg, setMsg] = useState('Think of a Question')
+
+    return <div onClick={postReply} style={{ backgroundColor: color}} id="EightBall"> {msg} </div>
 }
 
 export default EightBall;
