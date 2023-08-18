2731b613cb24fec963688140ebafc67c98639be9
riomonet <arizablozki@gmail.com>
2023-08-17 23:04:23 -0400

    set up basic css for ball

---
+++ src/EightBall.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Answers from './answers'
+import './EightBall.css'
+
+const EightBall = () => {
+    return <div id="EightBall"> Think of a Question </div>
+}
+
+export default EightBall;
+
+
